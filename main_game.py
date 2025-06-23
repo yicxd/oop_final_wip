c7f54c890bf0536b0ce998265ab61d38d01c0928
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-06-24 00:42:20 +0800

    game is played on terminal

--- main_game.py
+++ main_game.py
@@ -2,3 +2,28 @@ import curses
 import time
 import sys
 from random import randint
+from field import Field
+from snake import Snake
+
+
+def main(screen):
+    # Configure screen
+    screen.timeout(0)
+
+    # Init snake & field
+    field = Field(10)
+    snake = Snake("Joe")
+    snake.set_field(field)
+
+    while True:
+        ch = screen.getch()
+        if ch != -1:
+            snake.set_direction(ch)
+
+        snake.move()
+        field.render(screen)
+        screen.refresh()
+        time.sleep(.4)
+
+if __name__ == '__main__':
+    curses.wrapper(main)
