bb72a49e69adf0baf5fbed17e447efaa6601b6f9
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-06-23 19:45:27 +0800

    to know where you are and where you should be

--- game_object.py
+++ game_object.py
@@ -2,3 +2,9 @@ class GameObject:
     def __init__(self, name):
         self.name = name
         self.position = [0, 0]
+
+    def set_position(self, x, y):
+        self.position = [x, y]
+
+    def get_position(self):
+        return self.position
